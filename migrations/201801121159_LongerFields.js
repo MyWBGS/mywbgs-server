eab06be2064a35b49313f75c69e348a9c3dbc127
Nicholas Bush <nickdbush@hotmail.co.uk>
2018-09-03 12:12:38 +0100

    Delete 201801121159_LongerFields.js

--- migrations/201801121159_LongerFields.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    up: async (query, DataTypes) => {
-        await query.changeColumn('assignments', 'title', {type: DataTypes.TEXT});  
-        await query.changeColumn('assignments', 'notes', {type: DataTypes.TEXT});
-    }
-};
